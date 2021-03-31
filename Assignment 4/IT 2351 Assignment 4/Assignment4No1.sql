599189825fafb08fb761a67451e375b41ba8ba22
Ishani <31428274+ishani216@users.noreply.github.com>
2021-03-30 22:33:57 -0400

    Delete Assignment 4/IT 2351 Assignment 4 directory

--- Assignment 4/IT 2351 Assignment 4/Assignment4No1.sql
+++
@@ -1,9 +0,0 @@
--- *****************************************************
--- Ishani Nanavaty, Spring 2021
--- IT 2351 Assignment 4 No1 Prof. Tamerlano
-/*This query used format, convert, and cast functions and rounds the decimal point*/
--- *****************************************************
-SELECT list_price, FORMAT(list_price, 1) AS List_Price,
-CONVERT(list_price, SIGNED) AS Price,
-CAST(list_price AS SIGNED) AS Price
-FROM products
